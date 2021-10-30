60ac627eba5adf1e57fb1c0cd3c0d21ca64cf8b4
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2021-10-30 16:29:41 +0200

    feat: :zap: read totals_per_area.csv

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import os
+import json
+import urllib.request
+import pandas as pd
+import numpy as np
+
+from sqlalchemy import create_engine
+
+
 if __name__ == '__main__':
-    pass
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    df = pd.read_csv(
+        "../venezia-project-ssd/data/Museo M9/totals_per_area.csv")
+    timestamp = df.timestamp
+    areaCode = df.area_code
+    totals = df.totals
+    alarms = df.alarms
+    date = df.date
+
+    print(timestamp)
+    print(areaCode)
+    print(totals)
+    print(alarms)
+    print(date)
+    print("fine")
