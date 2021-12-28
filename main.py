f5c1f0bd32e85c72bebabe699af79d48b6c875dd
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2021-12-28 20:49:25 +0100

    refactor: :fire: clean main code

--- main.py
+++ main.py
@@ -1,99 +1,6 @@
-import math
-from datetime import datetime
-
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from matplotlib.pyplot import figure
-from pymongo import MongoClient
-from statsmodels.graphics.tsaplots import plot_acf
-import pmdarima as pm
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.stattools import kpss
-from keras.layers import Dense  # pip install tensorflow (as administrator)
-from keras.models import Sequential  # pip install keras
-
-from config import CONNECTION_STRING
-
-
-# from series of values to windows matrix
-def create_dataset(dataset, look_back=1):
-    dataX, dataY = [], []
-    for i in range(len(dataset) - look_back):
-        a = dataset[i:(i + look_back), 0]
-        dataX.append(a)
-        dataY.append(dataset[i + look_back, 0])
-
-    return np.array(dataX), np.array(dataY)
-
-def get_range_by_date(data, start, finish):
-    data["date"] = pd.to_datetime(data.date)
-    mask = (data.date > start) & (data.date <= finish)
-    return data.loc[mask]
-
-
-def plot_original_data(date, data, rows, columns, i, color, label):
-    plt.subplot(rows, columns, i)
-    plt.plot(date, data, color=color, label=label)
-    plt.legend()
-    plt.show()
-
-
-def autocorrelation(data):
-    plot_acf(data, lags=40, alpha=0.05)
-    plot_acf(data.diff().dropna(), lags=40, alpha=0.05)
-    plt.show()
-
-
-def compute_mean_and_variance(data):
-    X = data
-    split = round(len(X) / 2)
-    X1, X2 = X[0:split], X[split:]
-    mean1, mean2 = X1.mean(), X2.mean()
-    var1, var2 = X1.var(), X2.var()
-    print('mean1=%f, mean2=%f' % (mean1, mean2))
-    print('variance1=%f, variance2=%f' % (var1, var2))
-
-
-def function_kpss(data):
-    t_stat, p_value, _, critical_values = kpss(data, nlags='auto')
-
-    print(f'ADF Statistic: {t_stat:.2f}')
-    for key, value in critical_values.items():
-        print('Critial Values:')
-        print(f'{key}, {value:.2f}')
-
-    print(f'\np-value: {p_value:.2f}')
-    print("Stationary") if p_value > 0.05 else print("Non-Stationary")
-
-
-def adf_fuller(data):
-    t_stat, p_value, _, _, critical_values, _ = adfuller(data, autolag='AIC')
-    print(f'ADF Statistic: {t_stat:.2f}')
-    for key, value in critical_values.items():
-        print('Critial Values:')
-        print(f'{key}, {value:.2f}')
-
-    print(f'\np-value: {p_value:.2f}')
-    print("Non-Stationary") if p_value > 0.05 else print("Stationary")
-
-
-def connect_to_db():
-    return MongoClient(CONNECTION_STRING)
-
-
-def convert_timestamp(df):
-    date = []
-
-    for t in df.timestamp:
-        t = int(t)
-        ts = int(t / 1000)
-        time_value = datetime.fromtimestamp(ts)
-        date.append(time_value)
-
-    return date
-
+from functions import *
+from arima import auto_arima
+from mlp import mlp_forecast
 
 if __name__ == '__main__':
     """ Connect to DB """
@@ -209,57 +116,12 @@ if __name__ == '__main__':
     # # plt.legend()
 
     """ Forecasting with Statistical model """
-    # model = pm.auto_arima(df_floor1.totals.values, test='adf', start_p=1, start_q=1,
-    #                       max_p=3, max_q=3, m=1, d=None, D=None,
-    #                       seasonal=False, stationary=True, start_P=0, trace=True, stepwise=True)
-    #
-    # print(model.summary())
-    # fitted = model.fit(df_floor1.totals)
-    # yfore = fitted.predict(n_periods=1000)  # forecast
-    # ypred = fitted.predict_in_sample()
-    # plt.plot(df_floor1.totals, color="blue", label="Data")
-    # plt.plot(ypred, color="green", label="Prediction")
-    # plt.plot([None for i in ypred] + [x for x in yfore], color="red", label="Forecasting")
-    # plt.legend()
-    # plt.show()
-    #
-    # model.plot_diagnostics(figsize=(7, 5))
-    # plt.show()
-
+    auto_arima(df_floor1)
+    #auto_arima(df_floor2)
+    #auto_arima(df_floor3)
     """ Forecasting with Machine Learning model """
 
     """ Forecasting with Neural model """
-    look_back = 1008
-    train_size = int(len(df_floor1.totals) - look_back)
-    test_size = len(df_floor1.totals) - train_size
-    train, test = df_floor1.totals[0:train_size, :], df_floor1.totals[train_size:len(df_floor1.totals), :]
-
-    trainX, trainY = create_dataset(train, look_back)
-    testX, testY = create_dataset(test, look_back)
-
-    # Define model
-    model = Sequential()
-    model.add(Dense(32, input_dim=look_back, activation='relu'))  # 8 hidden neurons
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1))  # 1 output neuron
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=500, verbose=2)
-
-    # Estimate model performance
-    trainScore = model.evaluate(trainX, trainY, verbose=0)
-    print('Train Score: MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(trainScore,
-                                                               math.sqrt(trainScore)))
-
-    testScore = model.evaluate(testX, testY, verbose=0)
-
-    print('Test Score: MSE: {0:0.3f} RMSE: ({1:0.3f})'.format(testScore, math.sqrt(testScore)))
-    # generate predictions for training and forecast for plotting
-    trainPredict = model.predict(trainX)
-    testForecast = model.predict(testX)
-
-    plt.plot(df_floor1.totals, label="data")
-    plt.plot(np.concatenate((np.full(look_back - 1, np.nan), trainPredict[:, 0])), label="Prediction")
-    plt.plot(np.concatenate((np.full(len(train) - 1, np.nan), testForecast[:, 0])), label="Forecasting")
-    plt.legend()
-    plt.show()
+    mlp_forecast(df_floor1)
+    #mlp_forecast(df_floor2)
+    #mlp_forecast(df_floor3)
