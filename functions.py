99bda1a579babc59295854de408953a756470144
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2021-12-29 18:54:54 +0100

    feat: :construction: add random forest functions

--- functions.py
+++ functions.py
@@ -1,18 +1,16 @@
-import math
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
+from keras.losses import mean_absolute_error
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import figure
+from numpy.ma import asarray
+from pandas import DataFrame, concat
 from pymongo import MongoClient
+from sklearn.ensemble import RandomForestRegressor
 from statsmodels.graphics.tsaplots import plot_acf
-import pmdarima as pm
-from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.stattools import kpss
-from keras.layers import Dense  # pip install tensorflow (as administrator)
-from keras.models import Sequential  # pip install keras
 
 from config import CONNECTION_STRING
 
@@ -93,4 +91,62 @@ def convert_timestamp(df):
         date.append(time_value)
 
     return date
+
+    # transform a time series dataset into a supervised learning dataset
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+        n_vars = 1 if type(data) is list else data.shape[0]
+        df = DataFrame(data)
+        cols = list()
+        # input sequence (t-n, ... t-1)
+        for i in range(n_in, 0, -1):
+            cols.append(df.shift(i))
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, n_out):
+            cols.append(df.shift(-i))
+        # put it all together
+        agg = concat(cols, axis=1)
+        # drop rows with NaN values
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg.values
+
+# split a univariate dataset into train/test sets
+def train_test_split(data, n_test):
+        return data[:-n_test], data[-n_test:]
+
+# fit an random forest model and make a one step prediction
+def random_forest_forecast(train, testX):
+        # transform list into array
+        train = asarray(train)
+        # split into input and output columns
+        trainX, trainy = train[:, :-1], train[:, -1]
+        # fit model
+        model = RandomForestRegressor(n_estimators=1000)
+        model.fit(trainX, trainy)
+        # make a one-step prediction
+        yhat = model.predict([testX])
+        return yhat[0]
+
+# walk-forward validation for univariate data
+def walk_forward_validation(data, n_test):
+        predictions = list()
+        # split dataset
+        train, test = train_test_split(data, n_test)
+        # seed history with training dataset
+        history = [x for x in train]
+        # step over each time-step in the test set
+        for i in range(len(test)):
+            # split test row into input and output columns
+            testX, testy = test[i, :-1], test[i, -1]
+            # fit model on history and make a prediction
+            yhat = random_forest_forecast(history, testX)
+            # store forecast in list of predictions
+            predictions.append(yhat)
+            # add actual observation to history for the next loop
+            history.append(test[i])
+            # summarize progress
+            print('>expected=%.1f, predicted=%.1f' % (testy, yhat))
+        # estimate prediction error
+        error = mean_absolute_error(test[:, -1], predictions)
+        return error, test[:, -1], predictions
 
