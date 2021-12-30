e71adf09f5d38a2842306be3d9bffe16a4a41adc
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2021-12-30 19:32:50 +0100

    feat: :construction: floor_2 to floor_1 tests

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 from arima import auto_arima
-from constants import MONTH_LOOK_BACK
+from constants import *
 from functions import *
+from mlp import mlp_forecast
+from sarimax import auto_arima_for_sarimax
 
 if __name__ == '__main__':
     """ Connect to DB """
@@ -15,37 +17,37 @@ if __name__ == '__main__':
     total_per_area_sorted_collection = db_sorted["totals-per-area-sorted"]
 
     """ Find by area code"""
-    # cursors_floor_1 = total_per_area_collection.find({"area_code": "floor_1"})
-    cursors_floor_2 = total_per_area_collection.find({"area_code": "floor_2"})
+    cursors_floor_1 = total_per_area_collection.find({"area_code": "floor_1"})
+    # cursors_floor_2 = total_per_area_collection.find({"area_code": "floor_2"})
     # cursors_floor_3 = total_per_area_collection.find({"area_code": "floor_3"})
 
     """ Convert to dataframe """
-    # df_floor1 = pd.DataFrame(list(cursors_floor_1)) # Timestamp every 2 seconds
-    # df_floor1["totals"] = df_floor1["totals"].astype("int32")
+    df_floor1 = pd.DataFrame(list(cursors_floor_1)) # Timestamp every 2 seconds
+    df_floor1["totals"] = df_floor1["totals"].astype("int32")
 
-    df_floor2 = pd.DataFrame(list(cursors_floor_2))  # Timestamp every 1 minute
-    df_floor2["totals"] = df_floor2["totals"].astype("int32")
+    # df_floor2 = pd.DataFrame(list(cursors_floor_2))  # Timestamp every 1 minute
+    # df_floor2["totals"] = df_floor2["totals"].astype("int32")
     # df_floor3 = pd.DataFrame(list(cursors_floor_3))
     # df_floor3["totals"] = df_floor3["totals"].astype("int32")
 
     """ Sorting by timestamp and getting each N data to compact time series """
-    # df_floor1 = df_floor1.sort_values(by="timestamp", ascending=True)
-    # df_floor1 = df_floor1.iloc[1::300, :]  # Every 10 minutes
+    df_floor1 = df_floor1.sort_values(by="timestamp", ascending=True)
+    df_floor1 = df_floor1.iloc[1::300, :]  # Every 10 minutes
 
-    df_floor2 = df_floor2.sort_values(by="timestamp", ascending=True)
-    df_floor2 = df_floor2.iloc[1::10, :]  # Every 10 minutes
+    # df_floor2 = df_floor2.sort_values(by="timestamp", ascending=True)
+    # df_floor2 = df_floor2.iloc[1::10, :]  # Every 10 minutes
 
     # df_floor3 = df_floor3.sort_values(by="timestamp", ascending=True)
     # # df_floor3 = df_floor3.iloc[1::300, :]
 
     """ Convert timestamp to date and add replace 'date' to dataframe """
-    # date_val_floor_1 = convert_timestamp(df_floor1)
-    # df_floor1["date"] = date_val_floor_1
-    # df_floor1.drop(['_id', 'timestamp', 'area_code', 'alarms'], axis=1, inplace=True)  # Remove useless columns
+    date_val_floor_1 = convert_timestamp(df_floor1)
+    df_floor1["date"] = date_val_floor_1
+    df_floor1.drop(['_id', 'timestamp', 'area_code', 'alarms'], axis=1, inplace=True)  # Remove useless columns
 
-    date_val_floor_2 = convert_timestamp(df_floor2)
-    df_floor2["date"] = date_val_floor_2
-    df_floor2.drop(['_id', 'timestamp', 'area_code', 'alarms'], axis=1, inplace=True)  # Remove useless columns
+    # date_val_floor_2 = convert_timestamp(df_floor2)
+    # df_floor2["date"] = date_val_floor_2
+    # df_floor2.drop(['_id', 'timestamp', 'area_code', 'alarms'], axis=1, inplace=True)  # Remove useless columns
 
     # date_val_floor_3 = convert_timestamp(df_floor3)
     # df_floor3["date"] = date_val_floor_3
@@ -57,15 +59,15 @@ if __name__ == '__main__':
     # df_floor3 = get_range_by_date(df_floor3.totals, '2021-9-1', '2021-10-1')
 
     """ Redefining index """
-    # df_floor1.index = np.arange(0, len(df_floor1))
-    df_floor2.index = np.arange(0, len(df_floor2))
+    df_floor1.index = np.arange(0, len(df_floor1))
+    # df_floor2.index = np.arange(0, len(df_floor2))
     # df_floor3.index = np.arange(0, len(df_floor3))
 
     """ Plot original data """
     # figure(figsize=(15, 8))
-    # plot_original_data(df_floor1.date, df_floor1.totals, 2, 2, 1, "green", "Floor 1")
+    plot_original_data(df_floor1.date, df_floor1.totals, 2, 2, 1, "green", "Floor 1")
 
-    plot_original_data(df_floor2.date, df_floor2.totals, 2, 2, 2, "red", "Floor 2")
+    # plot_original_data(df_floor2.date, df_floor2.totals, 2, 2, 2, "red", "Floor 2")
     # plot_original_data(df_floor3.date, df_floor3.totals, 2, 2, 3, "blue", "Floor 3")
 
     # plt.subplot(2, 2, 4)
@@ -79,37 +81,37 @@ if __name__ == '__main__':
     # plt.legend()
 
     """ Autocorrelation """
-    # autocorrelation(df_floor1.totals)
-    autocorrelation(df_floor2.totals)
+    autocorrelation(df_floor1.totals)
+    # autocorrelation(df_floor2.totals)
     # autocorrelation(df_floor3.totals)
 
     """ ADF """
-    # adf_fuller(df_floor1.totals)
-    adf_fuller(df_floor2.totals)
+    adf_fuller(df_floor1.totals)
+    # adf_fuller(df_floor2.totals)
     # adf_fuller(df_floor3.totals)
 
     """ KPPS """
-    # function_kpss(df_floor1.totals)
-    function_kpss(df_floor2.totals)
+    function_kpss(df_floor1.totals)
+    # function_kpss(df_floor2.totals)
     # function_kpss(df_floor3.totals)
 
     """ Compute meaning and variance """
-    # compute_mean_and_variance(df_floor1.totals)
-    compute_mean_and_variance(df_floor2.totals)
+    compute_mean_and_variance(df_floor1.totals)
+    # compute_mean_and_variance(df_floor2.totals)
     # compute_mean_and_variance(df_floor3.totals)
 
     """ Plot seasonal decompose of each area """
-    # ds_floor_1 = df_floor1[df_floor1.columns[0]]
-    # result = seasonal_decompose(ds_floor_1, model='additive', period=1)
-    # result.plot()
-    #
-    # plt.show()
-
-    ds_floor_2 = df_floor2[df_floor2.columns[0]]
-    result = seasonal_decompose(ds_floor_2, model='additive', period=1)
+    ds_floor_1 = df_floor1[df_floor1.columns[0]]
+    result = seasonal_decompose(ds_floor_1, model='additive', period=1)
     result.plot()
 
     plt.show()
+
+    # ds_floor_2 = df_floor2[df_floor2.columns[0]]
+    # result = seasonal_decompose(ds_floor_2, model='additive', period=1)
+    # result.plot()
+    #
+    # plt.show()
     #
     # ds_floor_3 = ds_floor_3[ds_floor_3.columns[0]]
     # result = seasonal_decompose(ds_floor_3, model='additive', period=1)
@@ -119,13 +121,15 @@ if __name__ == '__main__':
 
     """ Forecasting with Statistical model """
     # auto_arima(df_floor1, MONTH_LOOK_BACK)
-    auto_arima(df_floor2, MONTH_LOOK_BACK)
+    # auto_arima(df_floor2, MONTH_LOOK_BACK)
     # auto_arima(df_floor3, MONTH_LOOK_BACK)
+
+    #auto_arima_for_sarimax(df_floor1, WEEK_LOOK_BACK)
     """ Forecasting with Machine Learning model """
     # random_forest(df_floor1, WEEK_LOOK_BACK)
 
     """ Forecasting with Neural model """
-    # mlp_forecast(df_floor1,WEEK_LOOK_BACK)
+    mlp_forecast(df_floor1,WEEK_LOOK_BACK)
     # mlp_forecast(df_floor2,WEEK_LOOK_BACK)
     # mlp_forecast(df_floor3,WEEK_LOOK_BACK)
 
